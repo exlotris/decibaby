66446d939dda6f64adeaaf5d66183b86437bd74f
exlotris <32177147+exlotris@users.noreply.github.com>
2018-02-03 14:12:46 +0100

    up

--- Leq_calculation.c
+++ Leq_calculation.c
@@ -36,70 +36,71 @@ double leqmax=0; //Valeur du leqmax initiale
 double sum10=0; //
 
 
-int cmpfunc (const void * a, const void * b) {
-   return ( *(int*)a - *(int*)b );
-}
+
 
 //*********************************************************************************
 //*********************************************************************************
 int main()
-{
-//                                      // read the value from the micro after filtering:
-//for(int i=0; i<NbSample; i++)
-//  {
-//    microValue[i] = analogRead(microPin); // Lit les valeurs du micro et les stock dans le tableau microValue[],
-//  }                                    // ces valeurs sont sous forme de voltage
-
-                                      // Calcule la moyenne des valeurs de microValue
-int sum =0;
-for (int i=0; i<NbSample; i++)
-{ sum+= microValue[i]; }
-
-tableauValeurVolt[nouvelEmplacement]=(int)sum/NbSample;
-
-if (tableauValeurVolt[nbValeur-1] != 0)
   {
-    if (nouvelEmplacement != 0)
-      {
-        Running_Leq -= tableauValeurVolt[nouvelEmplacement-1];
-        Running_Leq += tableauValeurVolt[nouvelEmplacement];
-      }
-      else
-      {
-        Running_Leq -= tableauValeurVolt[nbValeur-1];
-        Running_Leq += tableauValeurVolt[nouvelEmplacement];
-      }
-  }
-else
-  {
-    Running_Leq += tableauValeurVolt[nouvelEmplacement];
-  }
-// Calule la running average du leq
+  //                                      // read the value from the micro after filtering:
+  //for(int i=0; i<NbSample; i++)
+  //  {
+  //    microValue[i] = analogRead(microPin); // Lit les valeurs du micro et les stock dans le tableau microValue[],
+  //  }                                    // ces valeurs sont sous forme de voltage
 
-nouvelEmplacement += 1;
-if (nouvelEmplacement==nbValeur) {
-  nouvelEmplacement=0;
+                                        // Calcule la moyenne des valeurs de microValue
+  int sum =0;
+  for (int i=0; i<NbSample; i++)
+  { sum+= microValue[i]; }
+
+  tableauValeurVolt[nouvelEmplacement]=(int)sum/NbSample;
+
+  if (tableauValeurVolt[nbValeur-1] != 0)
+    {
+      if (nouvelEmplacement != 0)
+        {
+          Running_Leq -= tableauValeurVolt[nouvelEmplacement-1];
+          Running_Leq += tableauValeurVolt[nouvelEmplacement];
+        }
+        else
+        {
+          Running_Leq -= tableauValeurVolt[nbValeur-1];
+          Running_Leq += tableauValeurVolt[nouvelEmplacement];
+        }
+    }
+  else
+    {
+      Running_Leq += tableauValeurVolt[nouvelEmplacement];
+    }
+  // Calule la running average du leq
+
+  nouvelEmplacement += 1;
+  if (nouvelEmplacement==nbValeur) {
+    nouvelEmplacement=0;
+  }
+  for(int i=0; i<nbValeur;i++){
+  tableauValeurVolt_leq10[i]=tableauValeurVolt[i];
+  }
+
+  // Trie le tableau tels que les valeurs les + grandes soient en premiere position
+  qsort(tableauValeurVolt_leq10, nbValeur, sizeof(int), cmpfunc);
+  int sum10 =0;
+  for (int i=0; i<(nbValeur/10); i++)
+    {
+      sum10+= tableauValeurVolt_leq10[i];
+    }
+
+
+                                        // Calcule le leq du tableau microValue
+  leq = 20*log10(Running_Leq/(nbValeur*V_0/1000));
+                                        // Modification du calcul du leq
+
+  leq10 = 20*log10(sum10/((nbValeur/10)*V_0/1000));
+
+                                        // Calcule le leqmax du tableau microValue
+  leqmax = 20*log10((tableauValeurVolt_leq10[0]+tableauValeurVolt_leq10[1])/(2*V_0/1000));
+   return(0);
 }
-for(int i=0; i<nbValeur;i++){
-tableauValeurVolt_leq10[i]=tableauValeurVolt[i];
-}
-
-// Trie le tableau tels que les valeurs les + grandes soient en premiere position
-qsort(tableauValeurVolt_leq10, nbValeur, sizeof(int), cmpfunc);
-int sum10 =0;
-for (int i=0; i<(nbValeur/10); i++)
-  {
-    sum10+= tableauValeurVolt_leq10[i];
-  }
-
-
-                                      // Calcule le leq du tableau microValue
-leq = 20*log10(Running_Leq/(nbValeur*V_0/1000));
-                                      // Modification du calcul du leq
-
-leq10 = 20*log10(sum10/((nbValeur/10)*V_0/1000));
-
-                                      // Calcule le leqmax du tableau microValue
-leqmax = 20*log10((tableauValeurVolt_leq10[0]+tableauValeurVolt_leq10[1])/(2*V_0/1000));
-return (0);
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
 }
