f3749e04a29f47405513648fc9e41e8ea6f146a7
exlotris <32177147+exlotris@users.noreply.github.com>
2018-02-03 14:47:31 +0100

    up

--- LeqCalculation.c
+++ LeqCalculation.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 double microValue[20] = { }; // Le nombre de valeurs considerees dans le calcul du Leq
-int NbSample = 20; // Doit avoir la meme valeur que microValue[]
+int NbSample = 2; // Doit avoir la meme valeur que microValue[]
 double tableauValeurVolt[300] = { };
 double tableauValeurVolt_leq10[300] = { };
 int nouvelEmplacement = 0;
@@ -24,51 +24,72 @@ int main () {
   x = 5000;
   ret = log10(x);
   printf("log10(%lf) = %lf\n", x, ret);
-  sum = 0;
-  for (int i=0; i<NbSample; i++)
+
+
+
+
+  int array[2], i;
+  for(i = 0; i < 2; i++)
   {
-    sum+= microValue[i];
+    array[i] = (rand() % 100)+1;
+    printf("%4d\n", array[i]);
   }
 
-  tableauValeurVolt[nouvelEmplacement]=sum/NbSample;
-  if (tableauValeurVolt[nbValeur-1] != 0)
-  {
-    if (nouvelEmplacement != 0)
+
+
+  int looop=0;
+  while (looop>300) {
+    sum = 0;
+    for (int i=0; i<NbSample; i++)
     {
-      Running_Leq -= tableauValeurVolt[nouvelEmplacement-1];
-      Running_Leq += tableauValeurVolt[nouvelEmplacement];
+      sum+= array[i];
+    }
+
+    tableauValeurVolt[nouvelEmplacement]=sum/NbSample;
+    if (tableauValeurVolt[nbValeur-1] != 0)
+    {
+      if (nouvelEmplacement != 0)
+      {
+        Running_Leq -= tableauValeurVolt[nouvelEmplacement-1];
+        Running_Leq += tableauValeurVolt[nouvelEmplacement];
+      }
+      else
+      {
+        Running_Leq -= tableauValeurVolt[nbValeur-1];
+        Running_Leq += tableauValeurVolt[nouvelEmplacement];
+      }
     }
     else
     {
-      Running_Leq -= tableauValeurVolt[nbValeur-1];
       Running_Leq += tableauValeurVolt[nouvelEmplacement];
     }
-  }
-  else
-  {
-    Running_Leq += tableauValeurVolt[nouvelEmplacement];
-  }
-  // Calule la running average du leq
-  nouvelEmplacement += 1;
-  if (nouvelEmplacement==nbValeur)
-  {
-    nouvelEmplacement=0;
-  }
-  for(int i=0; i<nbValeur;i++)
-  {
-    tableauValeurVolt_leq10[i]=tableauValeurVolt[i];
-  }
+    // Calule la running average du leq
+    nouvelEmplacement += 1;
+    if (nouvelEmplacement==nbValeur)
+    {
+      nouvelEmplacement=0;
+    }
+    for(int i=0; i<nbValeur;i++)
+    {
+      tableauValeurVolt_leq10[i]=tableauValeurVolt[i];
+    }
 
-  // Trie le tableau tels que les valeurs les + grandes soient en premiere position
-  qsort(tableauValeurVolt_leq10, nbValeur, sizeof(int), cmpfunc);
-  int sum10 =0;
-  for (int i=0; i<(nbValeur/10); i++)
-  {
-    sum10+= tableauValeurVolt_leq10[i];
+    // Trie le tableau tels que les valeurs les + grandes soient en premiere position
+    qsort(tableauValeurVolt_leq10, nbValeur, sizeof(int), cmpfunc);
+    int sum10 =0;
+    for (int i=0; i<(nbValeur/10); i++)
+    {
+      sum10+= tableauValeurVolt_leq10[i];
+    }
+    // Calcule le leq du tableau microValue
+    leq = 20*log10(Running_Leq/(nbValeur*V_0));
+    leq10 = 20*log10(sum10/((nbValeur/10)*V_0));
+    leqmax = 20*log10((tableauValeurVolt_leq10[0]+tableauValeurVolt_leq10[1])/(2*V_0));
+    printf("%4d\n", leq);
+    printf("%4d\n", leq10);
+    printf("%4d\n", leqmax);
+    looop++;
+    delay(1000);
   }
-  // Calcule le leq du tableau microValue
-  leq = 20*log10(Running_Leq/(nbValeur*V_0));
-  leq10 = 20*log10(sum10/((nbValeur/10)*V_0));
-  leqmax = 20*log10((tableauValeurVolt_leq10[0]+tableauValeurVolt_leq10[1])/(2*V_0));
   return(0);
 }
