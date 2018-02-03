53939ef9786a67b979e160665adb2c821a24a202
exlotris <32177147+exlotris@users.noreply.github.com>
2018-02-03 14:19:24 +0100

    up

--- test.c
+++ test.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
+int microValue[20] = { }; // Le nombre de valeurs considerees dans le calcul du Leq
+int NbSample = 20; // Doit avoir la meme valeur que microValue[]
+int tableauValeurVolt[300] = { };
+int tableauValeurVolt_leq10[300] = { };
+int nouvelEmplacement = 0;
+int nbValeur = 300;
+int Running_Leq = 0;
+int V_0=1 ; //La tension correspondant au niveau zero Decibel (*1000)
+double leq=0; // Valeur du leq initiale
+double leq10=0; // Valeur du leq10 initiale
+double leqmax=0; //Valeur du leqmax initiale
+double sum10=0; //
+
 int main () {
    double x, ret;
    x = 10000;
